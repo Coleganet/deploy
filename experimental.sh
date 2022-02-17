3687fa7c4031d5cf778b062cef43bce74bd2b97c
coleganet <aalonso2003@gmail.com>
2022-02-17 02:04:18 +0000

    create install

--- experimental.sh
+++ experimental.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-#ssh-keygen -t rsa 
+ssh-keygen -t rsa 
 apt install docker docker.io 
-#sudo bash -c 'echo "{\"experimental\": true}" > /etc/docker/daemon.json'
-#sudo systemctl restart docker
+sudo bash -c 'echo "{\"experimental\": true}" > /etc/docker/daemon.json'
+sudo systemctl restart docker
 
 bash build.sh
 
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+apt install docker docker.io sudo
+sudo bash -c 'echo "{\"experimental\": true}" > /etc/docker/daemon.json'
+sudo systemctl restart docker
+sudo apt install curl
+output "installing the latest docker-compose"
+apt-get remove docker-compose && sudo rm /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+$SHELL
+docker–compose version
